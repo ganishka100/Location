19822ab0b0441e0930176cddce5a57606f4bcbae
Bhumika nathwani <131270195+ganishka100@users.noreply.github.com>
2024-02-29 13:11:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const getlocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        alert("Geolocation is not supported by this browser");
+    }
+};
+
+const showPosition = (position) => {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+
+    const locationDetails = document.getElementById("location-details");
+    locationDetails.innerText = `Latitude: ${lat}, Longitude: ${long}`;
+};
+
+const showError = (error) => {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred");
+            break;
+        default:
+            alert("An unknown error occurred");
+    }
+};
